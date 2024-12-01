74c6c956fe729c87d2b138b4cf399464790a8f74
siah-ai <siah@siah.ai>
2024-12-01 10:49:28 -0800

    Day2 prep

--- 2024/Siah/Day2/p1.ps1
+++ 2024/Siah/Day2/p1.ps1
@@ -1,1 +1,5 @@
-Get-Content ./input.txt
+$start = Get-Date
+$pinput = Get-Content .\input.txt
+
+Write-Host "Part 1: $gap"
+Write-Host "Elapsed time: $([math]::Round(((Get-Date) - $start).TotalSeconds,3)) seconds"
